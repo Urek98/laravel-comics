327934b3090ad174db41184939a18fcdb38320f6
Urek98 <gianluca.argo1998@libero.it>
2021-09-02 18:46:38 +0200

    terminata sezione comic

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="cover-bar">
-    
+
 </div>
 <section class="detail-section flex">
     <div class="description-section">
@@ -14,7 +14,7 @@
                 <div>U.S. Price:$19.99</div>
                 <div>AVABILE</div>
             </div>
-            <div class="check">Check Avability</div>
+            <div class="check">Check Availability</div>
         </div>
         <p>The celebration of 1,000 issues of Action Comics continues with a new, Deluxe Edition of the amazing comic that won raves when it hit comics shops in April! This hardcover includes all the stories from that issue, plus the tale by writer Paul Levitz and artist Neal Adams that appeared in the Action Comics: 80 Years Of Superman hardcover, as well as all the variant covers, design sketches by Jim Lee for Superman’s new look, scripts for the stories, the original art from the lost story featuring art by Curt Swan and more! Plus: a complete reprint of the stories that started it all—the Superman stories Action Comics #1 and 2 from 1938!</p>
     </div>
@@ -23,5 +23,37 @@
         <img src="/img/adv.jpg" alt="">
     </div>
 </section>
+<div class="about-section">
+    <div class="about-container flex">
+        <div class="talent">
+            <h2>Talent</h2>
+            <div class="art-by flex">
+                <div class="art-by-script">Art by:</div>
+                <div class="artist-writer">"José Luis García-López","Clay Mann","Rafael Albuquerque","Patrick Gleason","Dan Jurgens","Joe Shuster","Neal Adams","Curt Swan","John Cassaday","Olivier Coipel","Jim Lee"</div>
+            </div>
+            <div class="written-by flex">
+                <div class="written-by-script">Written by:</div>
+                <div class="artist-writer">"José Luis García-López","Clay Mann","Rafael Albuquerque","Patrick Gleason","Dan Jurgens","Joe Shuster","Neal Adams","Curt Swan","John Cassaday","Olivier Coipel","Jim Lee"</div>
+            </div>
+        </div>
+        <div class="specs">
+            <h2>Specs</h2>
+            <div class="art-by flex">
+                <div class="art-by-script">Series:</div>
+                <div class="artist-writer">ACTION COMIC</div>
+            </div>
+            <div class="art-by flex">
+                <div class="art-by-script">U.S. Price:</div>
+                <div class="artist-writer">$19.99</div>
+            </div>
+            <div class="art-by flex">
+                <div class="art-by-script">On sale date:</div>
+                <div class="artist-writer">Oct 02 2018</div>
+            </div>
+            
+        </div>
+
+    </div>
+</div>
 
 @endsection
