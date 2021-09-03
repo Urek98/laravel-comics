dded8d0cb2dc17d9ca9b5e5cdcad7112bcb1f7d2
Urek98 <gianluca.argo1998@libero.it>
2021-09-03 18:22:06 +0200

    improved comic page

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,21 +4,22 @@
 
 @section('content')
 <div class="cover-bar">
-
+    <img src="{{ $comics[$arrayIndex]['thumb'] }}" />
 </div>
+
 <section class="detail-section flex">
     <div class="description-section">
-        <h1>Action Comics #1000: The Deluxe Edition</h1>
+        <h1>{{ $comics[$arrayIndex]["title"] }}</h1>
         <div class="price-bar flex">
             <div class="price flex">
                 <div>U.S. Price:
-                    <span>price</span>
+                    <span>{{ $comics[$arrayIndex]["price"] }}</span>
                 </div>
-                <div>AVABILE</div>
+                <div>AVAILABLE</div>
             </div>
             <div class="check">Check Availability</div>
         </div>
-        <p>The celebration of 1,000 issues of Action Comics continues with a new, Deluxe Edition of the amazing comic that won raves when it hit comics shops in April! This hardcover includes all the stories from that issue, plus the tale by writer Paul Levitz and artist Neal Adams that appeared in the Action Comics: 80 Years Of Superman hardcover, as well as all the variant covers, design sketches by Jim Lee for Superman’s new look, scripts for the stories, the original art from the lost story featuring art by Curt Swan and more! Plus: a complete reprint of the stories that started it all—the Superman stories Action Comics #1 and 2 from 1938!</p>
+        <p>{{ $comics[$arrayIndex]["description"] }}</p>
     </div>
     <div class="ad-section">
         <div>ADVERTISEMENT</div>
@@ -46,13 +47,13 @@
             </div>
             <div class="art-by flex">
                 <div class="art-by-script">U.S. Price:</div>
-                <div class="artist-writer">$19.99</div>
+                <div class="artist-writer">{{ $comics[$arrayIndex]["price"] }}</div>
             </div>
             <div class="art-by flex">
                 <div class="art-by-script">On sale date:</div>
-                <div class="artist-writer">Oct 02 2018</div>
+                <div class="artist-writer">{{ $comics[$arrayIndex]["sale_date"] }}</div>
             </div>
-            
+
         </div>
 
     </div>
